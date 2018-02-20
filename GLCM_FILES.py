9bcd4dc7cc836220023c3a45c410f28f47404e70
lukkascost <lucas.costa@lit.ifce.edu.br>
2018-02-20 12:07:11 -0300

    glcm 2

--- GLCM_FILES.py
+++ GLCM_FILES.py
@@ -2,18 +2,22 @@ from MachineLearn.Classes.Extractors.GLCM import GLCM
 import numpy as np 
 import cv2
 new_file = []
-classes = [[1],[1],[1,2,3,4],[1,2,3,4]]
+classes = [[],[1],[1,2,3,4],[1,2,3,4]]
 for c,i in enumerate(classes):
         for p in i:
                 cont = 1
-                img = cv2.imread("../DataSet-Baumann/IMAGENS_RECORTADAS/c{}_p{}_{:05d}.JPG".format(c+1,p,cont), 0)
-                while(img!=None):
-                        print c+1, p,cont                      
-                        oGLCM = GLCM(img, 8)
-                        oGLCM.generateCoOccurenceHorizontal()
-                        oGLCM.normalizeCoOccurence()
-                        oGLCM.calculateAttributes()
-                        new_file.append(oGLCM.exportToClassfier("c{}_p{}".format(c+1,p)))
+                img = cv2.imread("../DataSet-Baumann/IMG_CORTADAS_ELIAS/c{}_p{}_{:05d}.JPG".format(c+1,p,cont), 0)
+                while(True):
+                        print c+1, p,cont                                              
+                        if (img==None and c>1):break
+                        elif (cont>400): break
+                        elif(img!=None):
+                                oGLCM = GLCM(img, 8)
+                                oGLCM.generateCoOccurenceHorizontal()
+                                oGLCM.normalizeCoOccurence()
+                                oGLCM.calculateAttributes()
+                                new_file.append(oGLCM.exportToClassfier("c{}_p{}".format(c+1,p)))
+                        
                         cont+=1
-                        img = cv2.imread("../DataSet-Baumann/IMAGENS_RECORTADAS/c{}_p{}_{:05d}.JPG".format(c+1,p,cont), 0)                            
-                        np.savetxt("03.txt",new_file, fmt="%s")
+                        img = cv2.imread("../DataSet-Baumann/IMG_CORTADAS_ELIAS/c{}_p{}_{:05d}.JPG".format(c+1,p,cont), 0)                            
+                        np.savetxt("GLCM_FILES/M1_CM8b_RE_ELIAS2.txt",new_file,delimiter=",", fmt="%s")
